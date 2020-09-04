f87ebb2117b3dffe1ee4ec47c41cd7c02ab55bdb
Arthur <ken.khanh@urbanos.io>
2020-09-04 16:12:38 +0700

    first commit

---
+++ src/wrapper/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../App.css';
+
+export default function index({children}) {
+    return (
+        <main className="svelte-wlazyz" style={{}}>
+            <div className="intro-container svelte-dmat7b">
+                {children}
+                <div className="hold svelte-dmat7b"></div>
+            </div>
+            <div className="notch svelte-wlazyz"><span className="svelte-wlazyz" /></div>
+            <div className="speaker svelte-wlazyz" />
+        </main>
+    )
+}
